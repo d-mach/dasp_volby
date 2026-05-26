ae3d9b08847e16308bcfe510dc996833d99933b8
Machek Daniela <machek@pse.cz>
2026-05-26 10:36:57 +0200

    data

--- main.py
+++ main.py
@@ -11,9 +11,6 @@ from bs4 import BeautifulSoup
 
 
 
-
-
-
 def main() -> None:
     if len(sys.argv) != 3 or not "https://" in sys.argv[1] or not ".csv" in sys.argv[2]:
         print("Použití: main.py <odkaz na územní celek> <jméno csv souboru>")
@@ -37,12 +34,29 @@ def main() -> None:
         # print(f"{base_url}{odk[1]}")
         response = requests.get(base_url+odk[1])
         soup = BeautifulSoup(response.text, features="html.parser") 
-        radek = [odk[0]]
+        radek = {"code": odk[0]}       
+        for h in soup.select('h3'):
+            location = h.get_text(strip=True)
+            if location.startswith("Obec: "):
+                radek["location"] = location[6:]
+
+        registered = soup.select_one('td.cislo[headers="sa2"]').get_text(strip=True)     
+        radek["registered"] = registered.replace("\xa0", " ")
+
+        envelopes = soup.select_one('td.cislo[headers="sa3"]').get_text(strip=True)   
+        radek["envelopes"] = envelopes.replace("\xa0", " ")
+
+        valid = soup.select_one('td.cislo[headers="sa6"]').get_text(strip=True)   
+        radek["valid"] = valid.replace("\xa0", " ")
+
+        rows = soup.select('table.table tr')
+        for row in rows:
+            party = row.select_one('td[headers="t1sa1 t1sb2"]')
+            votes = row.select_one('td[headers="t1sa2 t1sb3"]')
+            if party and votes:
+                radek[party.get_text(strip=True)] = votes.get_text(strip=True).replace("\xa0", " ")
+
         print(radek)
-        
-    
-
-
 
 # print(response.text)
     
